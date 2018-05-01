f2d04fbe0f45f30dc59de16faca5f9fbe2bd997c
wyq-mac <wangyaqi.chn@gmail.com>
2018-05-02 07:29:52 +0800

    button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function hello() {
-  console.log('hello')
+import Button from './components/button'
+
+const components = {
+  Button
 }
+
+const iview = {
+  ...components
+}
+
+const install = function(Vue) {
+  Object.keys(iview).forEach(key => {
+    Vue.component(key, iview[key])
+  })
+}
+
+install(window.Vue)
+
+const API = {
+  install
+}
+
+module.exports.default = module.exports = API
