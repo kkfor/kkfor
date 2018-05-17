748a784d5d7ffb1a9781e002d8eeccd563ecb9ac
wyq-mac <wangyaqi.chn@gmail.com>
2018-05-17 23:52:11 +0800

    icon

--- src/components/icon/index.vue
+++ src/components/icon/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <span></span>
+  <i :class="classes"></i>
 </template>
 <script>
+const prefixCls = 'fo-icon'
+
 export default {
-  
+  props: {
+    type: String
+  },
+  computed: {
+    classes() {
+      return `${prefixCls} ${prefixCls}-${this.type}`
+    }
+  }
 }
 </script>
